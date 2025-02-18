26a5dcdcd5f62651fc53d20b0b998f495bdeec84
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2025-02-18 22:35:08 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -15,10 +15,7 @@ const paypalRouter = require("./routes/Shop/paypalRoutes");
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((error) => console.log("❌ MongoDB Connection Error:", error));
 
